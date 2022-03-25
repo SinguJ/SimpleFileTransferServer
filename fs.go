e2820ed1f54bbd21d365f26f3feb4bd0bb0e29b4
Singu <singu@singu.top>
2022-03-25 16:12:03 +0800

    rename `getFiles` to `getMetadata` and refactored it

--- fs.go
+++ fs.go
@@ -50,18 +50,62 @@ func isDir(path string) bool {
     return stat.IsDir()
 }
 
-// 获取指定路径下的所有文件
-func getFiles(path string) ([]os.DirEntry, error) {
+// 获取指定路径的元数据
+// path: 目标的路径
+// recursion: 递归层级（若目标为文件夹，则该项控制向下读取多少层目录信息，0 表示不读取，1 表示只读取当前目录的子文件，-1 表示全部读取）
+func getMetadata(path string, recursion int) (Metadata, error) {
     // 计算完整路径
-    _path := filepath.Join(rootPath, path)
-
-    // 检查文件是否是文件夹
-    if !isDir(_path) {
-        return nil, ErrNotDir
+    realPath := filepath.Join(rootPath, path)
+    metadataImpl := MetadataImpl{
+        Name: filepath.Base(realPath),
+        Path: path,
     }
-
-    // 读取目录
-    return os.ReadDir(_path)
+    // 获取文件（或目录）的状态
+    stat, err := os.Stat(path)
+    if err != nil {
+        return nil, err
+    }
+    // 检查目标是否为文件夹
+    if !stat.IsDir() {
+        // 若不是，则构建该文件的元数据，并返回
+        metadataImpl.Type = TypeFile
+        return &File{
+            metadataImpl,
+            stat.Size(),
+            filepath.Ext(metadataImpl.Name),
+        }, nil
+    }
+    // 构建子文件的元数据
+    var files []Metadata = nil
+    if recursion == 0 {
+        files = make([]Metadata, 0)
+    } else {
+        var subRecursion int
+        if recursion == -1 {
+            subRecursion = -1
+        } else {
+            subRecursion = recursion - 1
+        }
+        var entries []os.DirEntry
+        entries, err = os.ReadDir(realPath)
+        if err != nil {
+            return nil, err
+        }
+        files = make([]Metadata, len(entries))
+        for index, entry := range entries {
+            subFilepath := filepath.Join(path, entry.Name())
+            files[index], err = getMetadata(subFilepath, subRecursion)
+            if err != nil {
+                return nil, err
+            }
+        }
+    }
+    // 构建目录的元数据
+    metadataImpl.Type = TypeDir
+    return &Directory{
+        metadataImpl,
+        files,
+    }, nil
 }
 
 func fileWriteTo(writer http.ResponseWriter, path string) {
