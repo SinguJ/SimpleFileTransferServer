dc0b70ff30230512fcd203f2255a8aa8cd74053e
Singu <singu@singu.top>
2022-03-25 11:22:35 +0800

    extract the readTargetPath function

--- api.go
+++ api.go
@@ -7,6 +7,8 @@ import (
     "net/http"
     "net/url"
     "os"
+    "path"
+    "strings"
 )
 
 func init() {
@@ -18,8 +20,8 @@ func init() {
 var ErrServiceOver = errors.New("服务结束")
 
 // 404
-func notFound(writer http.ResponseWriter, path string) {
-    _, err := fmt.Fprintln(writer, "找不到指定的文件或目录：", path[1:])
+func notFound(writer http.ResponseWriter, p string) {
+    _, err := fmt.Fprintln(writer, "找不到指定的文件或目录：", p[1:])
     if err != nil {
         log.Println(err)
     }
@@ -31,15 +33,48 @@ func serviceError(err interface{}) {
     panic(err)
 }
 
+// 读取请求的文件路径
+func readTargetPath(r *http.Request, prefix string) (targetPath string, err error) {
+    // 解析 URI
+    p, err := url.QueryUnescape(r.URL.Path)
+    if err != nil {
+        return "", err
+    }
+    // 去除接口前缀
+    if prefix != "" {
+        p = strings.TrimPrefix(p, prefix)
+    }
+
+    if p == "" {
+        targetPath = "/"
+        return
+    }
+    if p[0] != '/' {
+        p = "/" + p
+    }
+    targetPath = path.Clean(p)
+    // path.Clean removes trailing slash except for root;
+    // put the trailing slash back if necessary.
+    if p[len(p)-1] == '/' && targetPath != "/" {
+        // Fast path for common case of p being the string we want:
+        if len(p) == len(targetPath)+1 && strings.HasPrefix(p, targetPath) {
+            targetPath = p
+        } else {
+            targetPath += "/"
+        }
+    }
+    return
+}
+
 // 下载
-func download(writer http.ResponseWriter, path string) {
+func download(writer http.ResponseWriter, p string) {
     var err error
 
     // 获取路径的状态
-    stat, err := os.Stat(path)
+    stat, err := os.Stat(p)
     if err != nil {
         if os.IsNotExist(err) {
-            notFound(writer, path)
+            notFound(writer, p)
         }
         serviceError(err)
     }
@@ -48,26 +83,26 @@ func download(writer http.ResponseWriter, path string) {
         // TODO: 压缩下载
         serviceError("暂不支持下载文件夹")
     } else {
-        fileWriteTo(writer, path)
+        fileWriteTo(writer, p)
     }
 }
 
 // 展示文件或目录
-func show(writer http.ResponseWriter, path string) {
+func show(writer http.ResponseWriter, p string) {
     var err error
 
     // 获取路径的状态
-    stat, err := os.Stat(path)
+    stat, err := os.Stat(p)
     if err != nil {
         if os.IsNotExist(err) {
-            notFound(writer, path)
+            notFound(writer, p)
         }
         serviceError(err)
     }
     // 检查是否是目录
     if stat.IsDir() {
         // 展示文件列表
-        _, err := getFiles(path)
+        _, err := getFiles(p)
         if err != nil {
             return
         }
@@ -78,29 +113,29 @@ func show(writer http.ResponseWriter, path string) {
 }
 
 // 临时解决方法
-func downloadOrView(writer http.ResponseWriter, path string) {
+func downloadOrView(writer http.ResponseWriter, p string) {
     var err error
-    _path := getAbsPath(path)
+    _path := getAbsPath(p)
 
     // 获取路径的状态
     stat, err := os.Stat(_path)
     if err != nil {
         if os.IsNotExist(err) {
-            notFound(writer, path)
+            notFound(writer, p)
         }
         serviceError(err)
     }
     // 检查是否是目录
     if stat.IsDir() {
         // 展示文件列表
-        // err = viewFileList(writer, path)
+        // err = viewFileList(writer, p)
         // if err == nil {
         //     err = ErrServiceOver
         // }
         // serviceError(err)
     } else {
         // TODO: 展示文件摘要
-        fileWriteTo(writer, path)
+        fileWriteTo(writer, p)
     }
 }
 
@@ -121,12 +156,9 @@ func (h *Handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
     //     download(writer, request.RequestURI[9:])
     // }
     // show(writer, request.RequestURI)
-    path, err := url.QueryUnescape(request.RequestURI)
+    p, err := readTargetPath(request, "/api/download/")
     if err != nil {
         serviceError(err)
     }
-    if path == "" {
-        path = "/"
-    }
-    downloadOrView(writer, path)
+    downloadOrView(writer, p)
 }
