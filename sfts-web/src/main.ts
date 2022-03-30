e8103bb8bd93f8d36fbd8d55d0cebf65e70b29c5
Singu <singu@singu.top>
2022-03-30 15:40:48 +0800

    Register `axios` components
    Chinese:
    注册 `axios` 组件

--- sfts-web/src/main.ts
+++ sfts-web/src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+// English: Create Vue application instance
+// 汉语：创建 Vue 应用实例
+const app = createApp(App)
+
+// English: Register `axios` components
+// 汉语：注册 `axios` 组件
+app.use(VueAxios, axios)
+
+// English: Mount this Vue application instance on the `#app` node
+// 汉语：在 `#app` 节点上挂载这个 Vue 应用实例
+app.mount('#app')
