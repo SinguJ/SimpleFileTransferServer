ce4945c9d9b94b5223459b378ddb28493a07bc2c
Singu <singu@singu.top>
2022-03-30 14:52:22 +0800

    Configure component auto-import for Naive UI
    Chinese:
    配置 Naive UI 的组件自动导入

--- sfts-web/vite.config.ts
+++ sfts-web/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +9,10 @@ export default defineConfig({
     outDir: "../static",
     emptyOutDir: true,
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    }),
+  ],
 })
