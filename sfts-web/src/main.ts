01463f05dd81449fda33ad4bcc41eb48a5c9577e
Singu <singu@singu.top>
2022-03-31 17:58:01 +0800

    Add API function library and register to Vue global object
    Chinese:
    添加 API 的独立函数库，并将其注册到 Vue 全局对象中

--- sfts-web/src/main.ts
+++ sfts-web/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import ApiPlugin from './api'
 
 // English: Create Vue application instance
 // 汉语：创建 Vue 应用实例
@@ -10,6 +11,10 @@ const app = createApp(App)
 // English: Register `axios` components
 // 汉语：注册 `axios` 组件
 app.use(VueAxios, axios)
+
+// English: Register `api` components
+// 汉语：注册 `api` 组件
+app.use(ApiPlugin, axios)
 
 // English: Mount this Vue application instance on the `#app` node
 // 汉语：在 `#app` 节点上挂载这个 Vue 应用实例
